1c128d0768f16be9e7e39c4917a26a88e81c7ac8
eamytan <151121016+eamytan@users.noreply.github.com>
2023-11-17 14:26:06 +0800

    Update streamlit_app.py
    bugfixling line6

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Roket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg)
+streamlit.text('Hard-Boiled Free-Range Egg')
